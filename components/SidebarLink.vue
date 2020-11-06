d52fb86877b881648e9bc7355c655aaf9f246aa8
wangyonghao <wangyonghao@rongcloud.cn>
2020-11-06 11:18:58 +0800

    fix: sidebarLink 容错

--- components/SidebarLink.vue
+++ components/SidebarLink.vue
@@ -40,13 +40,17 @@ export default {
     const displayAllHeaders = $themeLocaleConfig.displayAllHeaders ||
       $themeConfig.displayAllHeaders
 
-    if (item.type === 'auto') {
-      return [link, renderChildren(h, item.children, item.basePath, $route, maxDepth)]
-    } else if ((active || displayAllHeaders) && item.headers && !hashRE.test(item.path)) {
-      const children = groupHeaders(item.headers)
-      return [link, renderChildren(h, children, item.path, $route, maxDepth)]
-    } else {
-      return link
+    try {
+      if (item.type === 'auto') {
+        return [link, renderChildren(h, item.children, item.basePath, $route, maxDepth)]
+      } else if ((active || displayAllHeaders) && item.headers && !hashRE.test(item.path)) {
+        const children = groupHeaders(item.headers)
+        return [link, renderChildren(h, children, item.path, $route, maxDepth)]
+      } else {
+        return link
+      }
+    } catch (e) {
+      return ''
     }
   }
 }
